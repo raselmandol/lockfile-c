be01a1b25d3174234bae777283b07370812f7d2e
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-07-16 21:33:45 +0530

    Create note.c

---
+++ advEncryption/note.c
@@ -0,0 +1,4 @@
+//The first program prompts the user to enter the filename of the file to be encrypted and the encryption key.
+// It then opens the input file for reading, creates a new file with the encrypted extension,
+//and reads each byte from the input file, encrypts it using the provided key, and writes it to the output file. 
+//The program uses a simple modulo operation to wrap around the character values and prevent them from exceeding the valid range.
