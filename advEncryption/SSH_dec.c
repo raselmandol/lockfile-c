96aa2118d5bcebc883668db5b0d472583ad2864f
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2024-01-29 21:34:58 +0530

    Update SSH_dec.c

--- advEncryption/SSH_dec.c
+++ advEncryption/SSH_dec.c
@@ -53,7 +53,7 @@ int main()
     const char* inputFileName = "output.enc";
     const char* outputFileName = "decrypted.txt";
     unsigned char key[] = "1234567890abcdef1234567890abcdef"; // 32 bytes for AES-256
-    unsigned char iv[] = "1234567890abcdef"; // 16 bytes for AES
+    unsigned char iv[] = "1234567890abcdef"; //16 bytes for AES
     OpenSSL_add_all_algorithms();
     decryptFile(inputFileName, outputFileName, key, iv);
     EVP_cleanup();
